30d9268df2c0976bed6998cf019cc11c92176c6f
Gregory Moore <thewatchfulone@gmail.com>
2019-04-10 12:04:08 -0500

    Initial commit

---
+++ GWMDatabase/GWMDatabase.h
@@ -0,0 +1,19 @@
+//
+//  GWMDatabase.h
+//  GWMDatabase
+//
+//  Created by Gregory Moore on 4/10/19.
+//  Copyright © 2019 Gregory Moore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GWMDatabase.
+FOUNDATION_EXPORT double GWMDatabaseVersionNumber;
+
+//! Project version string for GWMDatabase.
+FOUNDATION_EXPORT const unsigned char GWMDatabaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GWMDatabase/PublicHeader.h>
+
+
