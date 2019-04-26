49f8f3809ae1a0aa86044a214d6370259ac05fc9
Gregory Moore <thewatchfulone@gmail.com>
2019-04-26 00:13:32 -0500

    Update GWMRelationshipItem.h
    1. added some documentation

--- GWMDatabase/Model/GWMRelationshipItem.h
+++ GWMDatabase/Model/GWMRelationshipItem.h
@@ -10,17 +10,23 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+///@brief Represents the 'itemKey' column in a SQLite table.
 extern GWMColumnName const GWMTableColumnDataItemKey;
+///@brief Represents the 'relatedItemKey' column in a SQLite table.
 extern GWMColumnName const GWMTableColumnRelatedDataItemKey;
+///@brief Represents the 'relationshipKey' column in a SQLite table.
 extern GWMColumnName const GWMTableColumnRelationshipKey;
-
+/*!
+ * @class GWMRelationshipItem
+ * @discussion A GWMRelationshipItem object represents a row in a database relationship table. This class is usable as is, but you might wish to create a custom subclass.
+ */
 @interface GWMRelationshipItem : GWMDataItem
 
 @property (nonatomic, assign) NSInteger dataItemID;
 @property (nonatomic, assign) NSInteger relatedDataItemID;
 @property (nonatomic, assign) NSInteger relationshipID;
 
-+(instancetype)relationshipItemWithDataID:(NSInteger)dataItemID relatedID:(NSInteger)relatedID;
++(instancetype)relationshipItemWithDataID:(NSInteger)dataID relatedID:(NSInteger)relatedID;
 
 -(instancetype)initWithDataID:(NSInteger)dataID relatedID:(NSInteger)relatedID;
 
